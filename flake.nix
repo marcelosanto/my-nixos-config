51631808e9a8067d56612acc4a6611ac79d133c5
Marcelo Santos <rocha.mer21@gmail.com>
2025-09-28 19:33:08 -0300

    fix: Remove variável de ambiente desnecessária
    Remove WEBKIT_DISABLE_COMPOSITING_MODE, pois não é mais necessária.

--- flake.nix
+++ flake.nix
@@ -145,7 +145,6 @@
 
                     # 🚀 CORREÇÃO NVIDIA/WEBKIT: Aplicado na sessão Zsh
                     export WEBKIT_DISABLE_DMABUF_RENDERER=1
-                    export WEBKIT_DISABLE_COMPOSITING_MODE = 1;
                     export GDK_BACKEND="x11";
                   '';
                 };
